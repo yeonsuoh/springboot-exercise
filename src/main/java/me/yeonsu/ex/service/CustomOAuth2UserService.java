c2e16f668b7f5e64a9a33f6f957b661066fc9aee
OH YEONSU <lilylemonoh@naver.com>
2025-01-01 15:55:56 +0900

    [FEAT] Oauth2 Login - user db save

--- src/main/java/me/yeonsu/ex/service/CustomOAuth2UserService.java
+++ src/main/java/me/yeonsu/ex/service/CustomOAuth2UserService.java
@@ -4,8 +4,11 @@ import me.yeonsu.ex.dto.CustomOAuth2User;
 import me.yeonsu.ex.dto.GoogleResponse;
 import me.yeonsu.ex.dto.NaverResponse;
 import me.yeonsu.ex.dto.OAuth2Response;
+import me.yeonsu.ex.entity.UserEntity;
+import me.yeonsu.ex.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     // DefaultOAuth2UserService 는 OAuth2UserService 의 구현체
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -30,8 +39,29 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+
+        UserEntity existData = userRepository.findByUsername(username);
+
+        String role = null;
+        if (existData == null) {
+            UserEntity userEntity = new UserEntity();
+
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+        } else {
+            role = existData.getRole();
+
+            existData.setEmail(oAuth2Response.getEmail());
+
+            userRepository.save(existData);
+        }
+
         // 나머지 구현
-        String role = "ROLE_USER";
 
         return new CustomOAuth2User(oAuth2Response, role);
 
